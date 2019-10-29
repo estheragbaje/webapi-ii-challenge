82d81b710539f15462eb5dd47970997f71a0a681
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-10-29 16:11:27 +0100

    Completed POST request

--- index.js
+++ index.js
@@ -18,6 +18,70 @@ server.get("/api/posts", (req, res) => {
     });
 });
 
+server.post("/api/posts", (req, res) => {
+  const newPost = {
+    title: req.body.title,
+    contents: req.body.contents
+  };
+
+  db.insert(req.body)
+    .then(data => {
+      if (data) {
+        res.status(201).json(data);
+      } else {
+        res.status(400).json({
+          success: false,
+          errorMessage: "Please provide title and contents for the post."
+        });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        error: "There was an error while saving the post to the database"
+      });
+    });
+});
+
+server.post("/api/posts/:id/comments", (req, res) => {
+  db.insertComment(req.params.id)
+    .then(data => {
+      if (data) {
+        res.status(201).json(data);
+      } else {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        error: "There was an error while saving the comment to the database"
+      });
+    });
+});
+
+server.get("/api/posts/:id", (req, res) => {
+  db.findById(req.params.id)
+
+    .then(data => {
+      if (data) {
+        res.status(200).json(data);
+      } else {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        error: "The post information could not be retrieved."
+      });
+    });
+});
+
 server.listen(6000, () => {
   console.log("\n*** Server Running on http://localhost:6000 ***\n");
 });
