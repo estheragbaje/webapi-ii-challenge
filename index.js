aa852ed587f3f4b59e9131c5ce09bfa32882ce7d
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-10-29 17:36:56 +0100

    DELETE request created

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const db = require("./data/db");
 
 const server = express();
+server.use(express.json());
 
 server.get("/api/posts", (req, res) => {
   db.find()
@@ -19,10 +20,10 @@ server.get("/api/posts", (req, res) => {
 });
 
 server.post("/api/posts", (req, res) => {
-  const newPost = {
-    title: req.body.title,
-    contents: req.body.contents
-  };
+  //   const newPost = {
+  //     title: req.body.title,
+  //     contents: req.body.contents
+  //   };
 
   db.insert(req.body)
     .then(data => {
@@ -43,8 +44,15 @@ server.post("/api/posts", (req, res) => {
     });
 });
 
+//Creates a comment for the post with the specified
+//id using information sent inside of the request body.
 server.post("/api/posts/:id/comments", (req, res) => {
-  db.insertComment(req.params.id)
+  const comment = {
+    post_id: req.params.id,
+    text: req.body.text
+  };
+
+  db.insertComment(comment)
     .then(data => {
       if (data) {
         res.status(201).json(data);
@@ -58,6 +66,25 @@ server.post("/api/posts/:id/comments", (req, res) => {
       console.log(error);
       res.status(500).json({
         error: "There was an error while saving the comment to the database"
+      });
+    });
+});
+
+server.get("/api/posts/:id/comments", (req, res) => {
+  db.findCommentById(req.params.id)
+    .then(data => {
+      if (data) {
+        res.status(200).json(data);
+      } else {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        error: "The comments information could not be retrieved."
       });
     });
 });
@@ -78,6 +105,27 @@ server.get("/api/posts/:id", (req, res) => {
       console.log(error);
       res.status(500).json({
         error: "The post information could not be retrieved."
+      });
+    });
+});
+
+server.delete("/api/posts/:id", (req, res) => {
+  db.remove(req.params.id)
+    .then(deleted => {
+      if (deleted) {
+        res.status(204).end;
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "The user with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        success: false,
+        error: "The post could not be removed"
       });
     });
 });
