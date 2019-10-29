1a811c9853d0d8b1e32d426ae9bdee07f18254b3
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-10-29 14:42:20 +0100

    Created GET request endpoint for all posts

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ const db = require("./data/db");
 
 const server = express();
 
-server.get('/api/posts', (req, res) => {
-    db.find()
-})
+server.get("/api/posts", (req, res) => {
+  db.find()
+    .then(data => {
+      // console.log(data)
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        sucess: false,
+        error: "The posts information could not be retrieved."
+      });
+    });
+});
 
 server.listen(6000, () => {
-    console.log('\n*** Server Running on http://localhost:6000 ***\n');
-  });
-  
+  console.log("\n*** Server Running on http://localhost:6000 ***\n");
+});
